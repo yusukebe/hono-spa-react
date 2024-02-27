1f71afde1aeaef4d998247577cddb98761b21a54
Yusuke Wada <yusuke@kamawada.com>
2024-02-28 03:45:13 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+import { Hono } from 'hono'
+import { renderToString } from 'react-dom/server'
+
+const app = new Hono()
+
+app.get('/api/clock', (c) => {
+  return c.json({
+    time: new Date().toLocaleTimeString()
+  })
+})
+
+app.get('*', (c) => {
+  return c.html(
+    renderToString(
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" />
+          {import.meta.env.PROD ? (
+            <>
+              <script type="module" src="/static/client.js"></script>
+            </>
+          ) : (
+            <>
+              <script type="module" src="/src/client.tsx"></script>
+            </>
+          )}
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+    )
+  )
+})
+
+export default app
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import pages from '@hono/vite-cloudflare-pages'
+import devServer from '@hono/vite-dev-server'
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      build: {
+        rollupOptions: {
+          input: './src/client.tsx',
+          output: {
+            entryFileNames: 'static/client.js'
+          }
+        }
+      }
+    }
+  } else {
+    return {
+      ssr: {
+        external: ['react', 'react-dom']
+      },
+      plugins: [
+        pages(),
+        devServer({
+          entry: 'src/index.tsx'
+        })
+      ]
+    }
+  }
+})
